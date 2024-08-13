324a68a910a10c9b2175b1f9ec1b6da7d6d4d281
Rafa Tárrega <rafatarre@gmail.com>
2024-08-13 18:40:05 +0200

    feat: manifest app

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "TarrePassword",
   description: "Creating a password manager",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
